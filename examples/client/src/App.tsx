1fd251d83f1126dea301c26c99dade611bebefcf
Jacky Efendi <jacky.efendi1@gmail.com>
2019-10-02 15:22:11 +0700

    feat(examples/ssr): setup SSR-capable react app using razzle
    This will be used to show how react-isomorphic-data works during SSR

---
+++ examples/client/src/App.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useLazyData, useData } from 'react-isomorphic-data';
+
+import './styles.css';
+
+const App = () => {
+  const [fetchData, lazyData] = useLazyData('https://pokeapi.co/api/v2/pokemon/1/', {});
+  const eagerData = useData('https://pokeapi.co/api/v2/pokemon/2/', {});
+
+  return (
+    <div>
+      Example #1
+      <div>
+        <pre>
+          {JSON.stringify(eagerData, null, 2)}
+        </pre>
+      </div>
+      <hr />
+      Example #2
+      <button onClick={fetchData}>Hit me to fetch data!</button>
+      <div>
+        <pre>
+          {JSON.stringify(lazyData, null, 2)}
+        </pre>
+      </div>
+    </div>
+  );
+};
+
+export default App;
