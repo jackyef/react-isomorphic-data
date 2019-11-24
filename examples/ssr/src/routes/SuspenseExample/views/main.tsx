542ea2b95f6207281ec5eb378c3c0c83e480e0f2
Jacky Efendi <jacky.efendi1@gmail.com>
2019-11-24 16:54:37 +0700

    feat: added example for Suspense using HOC

--- examples/ssr/src/routes/SuspenseExample/views/main.tsx
+++ examples/ssr/src/routes/SuspenseExample/views/main.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import { DataResource } from 'react-isomorphic-data/dist/types/hooks/types';
+import SuspendedWithHOC from '../components/SuspendedWithHOC';
 
 const SuspenseMainView: React.SFC<{ resource: DataResource }> = ({ resource }) => {
   // We get the data here by calling `resource.read()`
   // If it's not ready, this component will suspend automatically
   const data = resource.read();
 
-  console.log({ data });
-
   return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <>
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+      <SuspendedWithHOC />
+    </>
   );
 };
 
