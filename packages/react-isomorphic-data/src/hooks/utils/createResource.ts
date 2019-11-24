f430504181d89e3c01cc5100c95b56d9c5b712db
Jacky Efendi <jacky.efendi1@gmail.com>
2019-11-24 16:58:26 +0700

    chore: cleanup logs

--- packages/react-isomorphic-data/src/hooks/utils/createResource.ts
+++ packages/react-isomorphic-data/src/hooks/utils/createResource.ts
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 const createResource = (loading: boolean, usedData: any, promiseRef: React.MutableRefObject<Promise<any> | null>) => {
   const read = () => {
-    console.log('read', {loading, promiseRef: promiseRef.current});
     if (loading && promiseRef.current instanceof Promise) {
       throw promiseRef.current;
     } else {
