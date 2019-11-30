51e72dbff0fd0351720779d8a4333a301b33c418
Jacky Efendi <jacky.efendi1@gmail.com>
2019-11-30 14:07:04 +0700

    improvement(example): updated example search input

--- examples/ssr/src/components/SearchInput.tsx
+++ examples/ssr/src/components/SearchInput.tsx
@@ -3,21 +3,20 @@ import { useData } from 'react-isomorphic-data';
 
 const SearchInput = () => {
   const [searchText, setSearchText] = React.useState('');
-  // const { data, error, loading } = useData('https://api.npms.io/v2/search/suggestions', {
-  //   q: searchText,
-  // });
-  const { data, error, loading } = useData('http://localhost:3000/some-rest-api', {
+  const { data, error, loading }: {
+    data: any,
+    error: Error | null,
+    loading: boolean,
+  } = useData('http://localhost:3000/some-rest-api', {
     q: searchText,
   });
-
-  console.log({ data, error, loading });
 
   return (
     <div>
       <input type="text" onChange={e => setSearchText(e.target.value)} />
       <pre>{JSON.stringify(data, null, 2)}</pre>
       {loading ? 'loading...' : null}
-      {/* {error ? error : null} */}
+      {error ? error.message : null}
     </div>
   )
 }
